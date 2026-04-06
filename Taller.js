e895a1001611dc23a5cd311525d75a4f5a7d2114
afcastanedas <afcastanedas@unbosque.edu.co>
2026-04-06 09:05:34 -0500

    Primera parte en clase
    Declaracion de variables y constantes

---
+++ Taller.js
@@ -0,0 +1,23 @@
+let Nombre=""
+let Edad=""
+/*const Tipo_de_documento={
+    RC: "Registro_Civil",
+    TI: "Tarjeta de identidad",
+    CC: "Cedula de ciudadania",
+    CE: "Cedula de extranjeria",
+    PP: "Pasaporte",
+}*/
+/*Punto 3*/
+let numero_de_documento=""
+let Salario=""
+let comisiones=""
+let Total_de_horas extras=""
+let Clasificación del nivel de riesgo=""
+/*Constantes*/
+const Salario_minimo_legal_vigente=1750905
+const salario_minimo_integral_vigente=22761765
+const subsidio_de_transporte=249095
+const Unidad_de_valor_tributario=5237
+
+
+
