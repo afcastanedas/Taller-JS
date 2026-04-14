1a9b4625527692c75d8b16cbde09387645d3d94b
afcastanedas <afcastanedas@unbosque.edu.co>
2026-04-14 07:52:40 -0500

    correccion en if
    correccion de comas que afectaban el if else

--- Taller.js
+++ Taller.js
@@ -1,5 +1,5 @@
-let Nombre=""
-let Edad=""
+let Nombre="";
+let Edad="";
 /*const Tipo_de_documento={
     RC: "Registro_Civil",
     TI: "Tarjeta de identidad",
@@ -8,18 +8,18 @@ let Edad=""
     PP: "Pasaporte",
 }*/
 /*Punto 3*/
-let numero_de_documento=""
-let Salario=""
-let comisiones=""
-let Total_de_horas_extras=""
-let Clasificación_del_nivel_de_riesgo=""
+let numero_de_documento="";
+let Salario="";
+let comisiones="";
+let Total_de_horas_extras="";
+let Clasificacion_del_nivel_de_riesgo="";
 /*Constantes*/
 const Salario_minimo_legal_vigente=1750905;
 const salario_minimo_integral_vigente=22761765;
 const subsidio_de_transporte=249095;
 const Unidad_de_valor_tributario=5237;
-const salud= *0.04;
-const pension= *0.04;
+const salud= 0.04;
+const pension= 0.04;
 const riesgo1= 0.522;
 const riesgo2= 1.044;
 const riesgo3= 2.436;
@@ -32,23 +32,18 @@ const fondo_de_solidaridad= 0.01;
 
 if (Edad<18){
     /*No se calcula*/
-};
-if (Edad>=18 && Edad<=25){
+} else if (Edad>=18 && Edad<=25){
     /*No se calcula porque es beneficiario*/
-};
-
-else if (Edad>25 && Edad<=60){
+} else if (Edad>25 && Edad<=60){
     /*Se calculan obligaciones*/
-};
-
-else if (Edad>60){
+} else if (Edad>60){
     /*Es pensionado*/
 };
 
 /*Calculo Variables*/
 
-let calculo_IBC= Salario+comisiones+(Total_de_horas_extras*Salario/240)
-let Calculo_de_salud= Salario*0.04
+let calculo_IBC= Salario+comisiones+(Total_de_horas_extras*Salario/240);
+let Calculo_de_salud= Salario * 0.04;
 let calculo_de_auxilio_de_transporte=
 
 
